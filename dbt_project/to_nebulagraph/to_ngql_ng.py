2fe9ba7b0b54fd065331525c862fc7e10477d031
HarrisChu <1726587+HarrisChu@users.noreply.github.com>
2024-08-01 16:52:22 +0800

    update

--- dbt_project/to_nebulagraph/to_ngql_ng.py
+++ dbt_project/to_nebulagraph/to_ngql_ng.py
@@ -24,41 +24,66 @@ CREATE GRAPH movie TYPED movie_type
 
 '''
 prefix_node = "USE movie INSERT OR REPLACE "
-prefix_edge =  "USE movie "
+prefix_edge = "USE movie "
+
+actor_act_movie_template = (
+    "table t{{user_id, movie_id}}={} "
+    "use movie for r in t match (a@`Actor`) where a.id=r.user_id "
+    "match (b@`Movie`) where b.id=r.movie_id insert or replace (a)-[@Act]->(b)"
+)
+
+director_direct_movie_template = (
+    "table t{{user_id, movie_id}}={} "
+    "use movie for r in t match (a@`Director`) where a.id=r.user_id "
+    "match (b@`Movie`) where b.id=r.movie_id insert or replace (a)-[@Direct]->(b)"
+)
+
+user_watched_movies_template = (
+    "table t{{user_id, movie_id, rate}}={} "
+    "use movie for r in t match (a@`User`) where a.id=r.user_id "
+    "match (b@`Movie`) where b.id=r.movie_id insert or replace (a)-[@Watch{{rate: r.rate}}]->(b)"
+)
+
+movie_withgenre_genre_template = (
+    "table t{{movie_id, genre_id}}={} "
+    "use movie for r in t match (a@`Movie`) where a.id=r.movie_id "
+    "match (b@`Genre`) where b.id=r.genre_id insert or replace (a)-[@WithGenre]->(b)"
+)
+
 
 def to_ngql(output):
     data = []
     data.append(schema)
     data.extend(convert_node_to_ngql("sub_movie.csv", prefix_node, row_fn_movie))
     data.extend(convert_node_to_ngql("sub_actor.csv", prefix_node, row_fn_actor))
-    data.extend(
-        convert_node_to_ngql("sub_director.csv", prefix_node, row_fn_director)
-    )
+    data.extend(convert_node_to_ngql("sub_director.csv", prefix_node, row_fn_director))
     data.extend(convert_node_to_ngql("sub_user.csv", prefix_node, row_fn_user))
-    data.extend(convert_node_to_ngql("sub_genre.csv",prefix_node, row_fn_genre))
+    data.extend(convert_node_to_ngql("sub_genre.csv", prefix_node, row_fn_genre))
     data.extend(
         convert_egde_to_ngql(
-            "sub_actor_act_movie.csv",prefix_edge, row_fn_act_movie
+            "sub_actor_act_movie.csv",
+            actor_act_movie_template,
+            row_fn_act_movie,
         )
     )
     data.extend(
         convert_egde_to_ngql(
             "sub_director_direct_movie.csv",
-           prefix_edge,
+            director_direct_movie_template,
             row_fn_direct_movie,
         )
     )
     data.extend(
         convert_egde_to_ngql(
             "sub_user_watched_movies.csv",
-            prefix_edge,
+            user_watched_movies_template,
             row_fn_watch_movie,
         )
     )
     data.extend(
         convert_egde_to_ngql(
             "sub_movie_withgenre_genre.csv",
-            prefix_edge,
+            movie_withgenre_genre_template,
             row_fn_withgenre,
         )
     )
@@ -67,8 +92,10 @@ def to_ngql(output):
         output_ngql.write(content)
 
 
-# USE movie INSERT OR IGNORE (@Actor{id:1,name:"player_1"), (@Actor{id:2,name:"player_1"), 
-def convert_node_to_ngql(input_file,prefix, row_fn, batch_size=256, ignore_header=True):
+# USE movie INSERT OR IGNORE (@Actor{id:1,name:"player_1"), (@Actor{id:2,name:"player_1"),
+def convert_node_to_ngql(
+    input_file, prefix, row_fn, batch_size=256, ignore_header=True
+):
     with open(input_file, "r") as input_csv:
         ignored = False
         csv_reader = csv.reader(input_csv)
@@ -90,8 +117,19 @@ def convert_node_to_ngql(input_file,prefix, row_fn, batch_size=256, ignore_heade
         return queries
 
 
-# USE movie MATCH (a:Person{id:1}),(b:Movie{id:1}) INSERT (a)-[:Direct{1}]->(b)
-def convert_egde_to_ngql(input_file, prefix, row_fn, batch_size=1, ignore_header=True):
+# table t{user_id, movie_id, rate}=
+# (305, 11, 5.0),
+# (304, 11, 3.5)
+# use movie
+# for r in t
+# return r.user_id as user_id, r.movie_id as movie_id, r.rate as rate
+# next
+# match (a@`User`) where a.id=user_id
+# match (b@`Movie`) where b.id=movie_id
+# insert or replace (a)-[@Watch{rate: rate}]->(b)
+def convert_egde_to_ngql(
+    input_file, template, row_fn, batch_size=256, ignore_header=True
+):
     with open(input_file, "r") as input_csv:
         csv_reader = csv.reader(input_csv)
         batch_cursor = 0
@@ -105,11 +143,13 @@ def convert_egde_to_ngql(input_file, prefix, row_fn, batch_size=1, ignore_header
             batch_cursor += 1
             batch.append(row_fn(row))
             if batch_cursor >= batch_size:
-                queries.append(prefix + ",".join(batch))
+                q = template.format(",".join(batch))
+                queries.append(q)
                 batch_cursor = 0
                 batch = []
         if batch_cursor > 0:
-            queries.append(prefix + ",".join(batch))
+            q = template.format(",".join(batch))
+            queries.append(q)
         return queries
 
 
@@ -120,16 +160,16 @@ def row_fn_movie(row):
 def row_fn_actor(row):
     # birthDate maybe \N sometimes
     if row[2] == "\\N":
-      return f'(@Actor{{id: {row[0][2:]}, name: "{row[1]}", birthDate: null}})'
+        return f'(@Actor{{id: {row[0][2:]}, name: "{row[1]}", birthDate: null}})'
     else:
-      return f'(@Actor{{id: {row[0][2:]}, name: "{row[1]}", birthDate: Date("{row[2]}")}})'
+        return f'(@Actor{{id: {row[0][2:]}, name: "{row[1]}", birthDate: Date("{row[2]}")}})'
 
 
 def row_fn_director(row):
     if row[2] == "\\N":
-      return f'(@Director{{id: {row[0][2:]}, name: "{row[1]}", birthDate: null}})'
+        return f'(@Director{{id: {row[0][2:]}, name: "{row[1]}", birthDate: null}})'
     else:
-      return f'(@Director{{id: {row[0][2:]}, name: "{row[1]}", birthDate: Date("{row[2]}")}})'
+        return f'(@Director{{id: {row[0][2:]}, name: "{row[1]}", birthDate: Date("{row[2]}")}})'
 
 
 def row_fn_user(row):
@@ -141,16 +181,16 @@ def row_fn_genre(row):
 
 
 def row_fn_act_movie(row):
-    return f'Match (a@Actor{{id: {row[0][2:]}}}), (b@Movie{{id: {row[1]}}}) Insert Or Replace (a)-[:Act]->(b)'
+    return f'({row[0][2:]}, {row[1]})'
 
 
 def row_fn_direct_movie(row):
-    return f'Match (a@Director{{id: {row[0][2:]}}}), (b@Movie{{id: {row[1]}}}) Insert Or Replace (a)-[:Direct]->(b)'
+    return f'({row[0][2:]}, {row[1]})'
 
 
 def row_fn_watch_movie(row):
-    return f'Match (a@`User`{{id: {row[0][2:]}}}), (b@Movie{{id: {row[2]}}}) Insert Or Replace (a)-[:Watch{{rate: {row[1]}}}]->(b)'
+    return f'({row[0][2:]}, {row[1]}, {row[2]})'
 
 
 def row_fn_withgenre(row):
-    return f'Match (a@Movie{{id: {row[0]}}}), (b@Genre{{id: {row[1][2:]}}}) Insert Or Replace (a)-[:WithGenre]->(b)'
+    return f'({row[0]}, {row[1][2:]})'
